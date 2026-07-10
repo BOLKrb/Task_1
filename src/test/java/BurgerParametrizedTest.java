3542f694c9e7fcbeb6bf4210b86a679b7ba2d89d
BOLK_rb <BOLKrb@yandex.ru>
2026-07-10 16:21:47 +0300

    fix Add BurgetParametrizedTest

--- src/test/java/BurgerParametrizedTest.java
+++ src/test/java/BurgerParametrizedTest.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import praktikum.Bun;
+import praktikum.Burger;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BurgerParametrizedTest {
+
+    private Burger burger;
+
+    private Ingredient ingredient;
+
+    private Bun bun;
+
+    @BeforeEach
+    void setUp() {
+        burger = new Burger();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "black bun, 100, SAUCE, Кетчуп, 50.2",
+            "white bun, 200, SAUCE, Кетчуп, 50.2",
+            "red bun, 300, SAUCE, Кетчуп, 50.2",
+            "black bun, 100, FILLING, Бекон, 200.5",
+            "white bun, 200, FILLING, Бекон, 200.5",
+            "red bun, 300, FILLING, Бекон, 200.5"
+    })
+    void createBurgerWithParamsShouldReturnBurger(String bunName, float bunPrice, IngredientType ingredientType, String ingredientName, float ingredientPrice) {
+        bun = new Bun(bunName, bunPrice);
+        ingredient = new Ingredient(ingredientType, ingredientName, ingredientPrice);
+
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+
+        assertAll(
+                () -> assertEquals(bunName, burger.bun.getName()),
+                () -> assertEquals(bunPrice, burger.bun.getPrice()),
+                () -> assertEquals(ingredientName, burger.ingredients.get(0).getName()),
+                () -> assertEquals(ingredientPrice, burger.ingredients.get(0).getPrice()),
+                () -> assertEquals(ingredientType, burger.ingredients.get(0).getType())
+        );
+    }
 }
